bcdb9defb9214d95e94b85e51ca1cd2403e26bc3
Aachlyss <ivanangelov05@gmail.com>
2023-10-20 16:31:10 +0300

    Finished
    Finished

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+#inputs
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+#f-string
+madlib = f"Computer programming is so {adj}!\I always get weird feelings and want to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+
+#printing the result
+print(madlib)
